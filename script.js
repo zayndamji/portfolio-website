113c2cf06f303140a89c22fc136ce89bd8c72f15
zaydam1000000 <86502397+zayndamji@users.noreply.github.com>
2025-06-17 20:33:52 -0700

    added typing animation to homepage

---
+++ script.js
@@ -0,0 +1,32 @@
+const roles = ["Programmer", "Developer", "Designer"];
+const textElement = document.getElementById("typewriter-text");
+const cursor = document.querySelector(".cursor");
+
+let roleIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+
+function type() {
+  const current = roles[roleIndex];
+  if (isDeleting) {
+    textElement.textContent = current.substring(0, charIndex--);
+  } else {
+    textElement.textContent = current.substring(0, charIndex++);
+  }
+
+  if (!isDeleting && charIndex === current.length+1) {
+    isDeleting = true;
+    setTimeout(type, 1000); // pause before deleting
+  } else if (isDeleting && charIndex === -1) {
+    isDeleting = false;
+    roleIndex = (roleIndex + 1) % roles.length;
+    setTimeout(type, 200);
+  } else {
+    setTimeout(type, isDeleting ? 25 : 100);
+  }
+
+  // debugging typing
+  // console.log({ content: textElement.textContent, isDeleting, charIndex, current, currentLength: current.length, roleIndex })
+}
+
+type();
